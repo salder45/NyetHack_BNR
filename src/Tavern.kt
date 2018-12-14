fe746bfb5b4b478245f948e18095d2a26037f3ca
Eder Martinez <edermtz.04@gmail.com>
2018-12-13 21:17:42 -0600

    Checking null with if

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
 //    beverage = null
+    if(beverage != null){
+        beverage = beverage.capitalize()
+    }else{
+        println("I can't do that without crashing - beverage was null!")
+    }
     println(beverage)
 }
