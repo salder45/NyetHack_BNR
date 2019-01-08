a1ea46cc6a1f90c369a566965ddbacf880e729b3
Eder Martinez <edermtz.04@gmail.com>
2019-01-08 08:07:01 -0600

    Chapter 7 Strings -> split

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,11 +1,16 @@
 const val TAVERN_NAME = "Taernly's Folly"
 
 fun main(args: Array<String>) {
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-private fun placeOrder(){
+private fun placeOrder(menuData: String){
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type,name,price) = menuData.split(',')
+
+    val message = "Madrigals buys a $name ($type) for $price"
+    println(message)
 }
