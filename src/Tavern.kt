43b0101245d68f7efce7e213c26b046ef8b0d19d
Eder Martinez <edermtz.04@gmail.com>
2019-01-08 21:41:02 -0600

    challenge improving dragon speak

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,18 +1,23 @@
 const val TAVERN_NAME = "Taernly's Folly"
 
 fun main(args: Array<String>) {
-    //placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("elixir, Shirley's Temple,4.12")
+    placeOrder("shandy,Dragon's Breath,5.91")
+    //placeOrder("elixir, Shirley's Temple,4.12")
 }
 
 private fun toDragonsSpeak(prhase: String)=
-    prhase.replace(Regex("[aeiou]")){
+    prhase.replace(Regex("[aeiouAEIOU]")){
         when(it.value){
             "a" -> "4"
             "e" -> "3"
             "i" -> "1"
             "o" -> "0"
             "u" -> "|_|"
+            "A" -> "4"
+            "E" -> "3"
+            "I" -> "1"
+            "O" -> "0"
+            "U" -> "|_|"
             else -> it.value
         }
     }
@@ -28,7 +33,7 @@ private fun placeOrder(menuData: String){
     println(message)
 
    val phrase = if (name == "Dragon's Breath"){
-       "Madrigal exclaims ${toDragonsSpeak("Ah, delicious $name!")}"
+       "Madrigal exclaims ${toDragonsSpeak("DRAGON'S BREATH: IT'S GOT WHAT ADVENTURERS CRAVE!")}"
    }else{
        "Madrigal says: Thank you for the $name."
    }
