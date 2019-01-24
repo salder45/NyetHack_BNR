ead20333c3daf3b0302e8a603dea3bb60fc97f9f
Eder Martinez <edermtz.04@gmail.com>
2019-01-23 21:57:07 -0600

    Chapter 12 -> Refactoring NyetHack

--- src/Game.kt
+++ src/Game.kt
@@ -1,52 +1,27 @@
 fun main(args: Array<String>) {
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
+
 
     val player = Player()
     player.castFireball()
 
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
-    printPlayerStatus(auraColor, isBlessed, player.name, healthStatus)
+    val auraColor = player.auraColor()
+    printPlayerStatus(player)
 
 
 
 }
 
 private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
+    player: Player
 ) {
     println(
-        "(Aura: $auraColor" +
-                "(Blessed: ${if (isBlessed) "YES" else "No"})"
+        "(Aura: ${player.auraColor()}" +
+                "(Blessed: ${if (player.isBlessed) "YES" else "No"})"
     )
     //Player status
-    println("$name $healthStatus")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
 
-    //Aura
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String =
-    when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
 
 
--- src/Player.kt
+++ src/Player.kt
@@ -5,6 +5,31 @@ class Player{
         field = value.trim()
     }
 
+    var healthPoints = 89
+    val isBlessed = true
+    private val isImmortal = false
+
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
+
+    fun auraColor(): String {
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+
+        //Aura
+        val auraColor = if (auraVisible) "GREEN" else "NONE"
+        return auraColor
+    }
+
+    fun formatHealthStatus(): String =
+        when (healthPoints) {
+            100 -> "is in excellent condition!"
+            in 90..99 -> "has a few scratches."
+            in 75..89 -> if (isBlessed) {
+                "has some minor wounds but is healing quite quickly!"
+            } else {
+                "has some minor wounds."
+            }
+            in 15..74 -> "looks pretty hurt."
+            else -> "is in awful condition!"
+        }
 }
