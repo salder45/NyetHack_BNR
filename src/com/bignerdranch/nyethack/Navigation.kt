8278f536310bd5341b257e65ebd5d10974d8d071
Eder Martinez <edermtz.04@gmail.com>
2019-01-29 21:21:49 -0600

    Chapter 15 -> Enumerarated classes

--- src/com/bignerdranch/nyethack/Navigation.kt
+++ src/com/bignerdranch/nyethack/Navigation.kt
@@ -1,5 +1,16 @@
 package com.bignerdranch.nyethack
 
+enum class Direction(private val coordinate: Coordinate){
+    NORTH(Coordinate(0,-1)),
+    EAST(Coordinate(1,0)),
+    SOUTH(Coordinate(0,1)),
+    WEST(Coordinate(-1,0))
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+            Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+
+}
+
 data class Coordinate(val x: Int, val y: Int){
     val isInBounds = x >= 0 && y >= 0
 }
