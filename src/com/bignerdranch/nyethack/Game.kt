9ccbe44446687893691253fbcac53743ca8c4bc1
Eder Martinez <edermtz.04@gmail.com>
2019-01-29 20:44:55 -0600

    Chapter 15 -> Nested classes

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -2,29 +2,60 @@ package com.bignerdranch.nyethack
 
 fun main(args: Array<String>) {
 
-
-    val player = Player("Madrigal")
-    player.castFireball()
-
-    val auraColor = player.auraColor()
-    printPlayerStatus(player)
-
-
-    var currentRoom = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
+    Game.play()
 
 }
 
-private fun printPlayerStatus(
-    player: Player
-) {
-    println(
-        "(Aura: ${player.auraColor()}" +
-                "(Blessed: ${if (player.isBlessed) "YES" else "No"})"
-    )
-    //com.bignerdranch.nyethack.Player status
-    println("${player.name} ${player.formatHealthStatus()}")
+
+
+object Game{
+
+    private val player = Player("Madrigal")
+    private var currentRoom = TownSquare()
+
+
+    init {
+        println("Welcome adventurer.")
+        player.castFireball()
+    }
+
+    fun play(){
+        while (true){
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            //Player status
+            printPlayerStatus(player)
+
+            //control section
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+    }
+
+    private fun printPlayerStatus(
+        player: Player
+    ) {
+        println(
+            "(Aura: ${player.auraColor()}" +
+                    "(Blessed: ${if (player.isBlessed) "YES" else "No"})"
+        )
+        //com.bignerdranch.nyethack.Player status
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1,{ "" })
+
+        fun processCommand() = when(command.toLowerCase()){
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+    }
+
 }
 
 
