0c24cd2370c9783802c40dbcefb05e0864faeeb1
Eder Martinez <edermtz.04@gmail.com>
2019-02-03 21:56:03 -0600

    Chapter 16 -> Combat on NyetHack

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.nyethack
 
 import java.lang.IllegalStateException
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
 
@@ -56,6 +57,7 @@ object Game{
         val argument = input.split(" ").getOrElse(1,{ "" })
 
         fun processCommand() = when(command.toLowerCase()){
+            "fight" -> fight()
             "move" -> move(argument)
             else -> commandNotFound()
         }
@@ -78,6 +80,30 @@ object Game{
             }catch (e: Exception){
                 "Invalid direction: $directionInput"
             }
+
+    private fun fight() = currentRoom.monster?.let {
+        while (player.healthPoints > 0 && it.healthPoints > 0){
+            slay(it)
+            Thread.sleep(1000)
+        }
+
+        "Combat complete."
+    }?: "There's nothing here to fight."
+
+    private fun slay(monster: Monster){
+        println("${monster.name} did ${monster.attack(player)} damage!")
+        println("${player.name} did ${monster.attack(monster)} damage!")
+
+        if(player.healthPoints <= 0){
+            println(">>>> You have been defeated! Thanks for playing. <<<<")
+            exitProcess(0)
+        }
+
+        if(monster.healthPoints <= 0){
+            println(">>>> ${monster.name} has been defeated! <<<<")
+            currentRoom.monster = null
+        }
+    }
 
 }
 
